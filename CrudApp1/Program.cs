5e17525351e19d0c10bdacb94d43f70ea6f8409a
NarminSH <nshivakhanova@gmail.com>
2022-09-10 17:30:10 +0400

    last changes

--- CrudApp1/Program.cs
+++ CrudApp1/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CrudApp1.Models;
+NorthwindContext context = new NorthwindContext();
 
+IGenericRepository<Category> repository = new GenericRepository<Category>(context);
+
+
+var collec = repository.GetById(1);
+repository.Delete(collec);
+collec.CategoryName = "New";
+repository.Update(collec);
+
+var all = repository.GetAll();
+foreach (var item in all)
+{
+    Console.WriteLine(item.CategoryName);
+}
